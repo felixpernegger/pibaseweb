93c0afb3608cb942ef9fd29f7e735f8c30379278
Batixx <s59fpern@uni-bonn.de>
2026-02-26 18:02:24 +0100

    undo deletion

--- packages/viewer/src/routes/+layout.server.ts
+++ packages/viewer/src/routes/+layout.server.ts
@@ -1,9 +1,10 @@
 import main from '../../public/refs/heads/main.json'
+import undecidable from '../../public/refs/heads/undecidable-data.json'
 
 const bundleHost = import.meta.env.VITE_BUNDLE_HOST
 
 export const load = () => {
   if (bundleHost?.includes('localhost')) {
-    return main
+    return undecidable
   }
 }
