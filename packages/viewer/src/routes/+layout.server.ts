ec92e8462b688517e10ed9e777c241b4f95bd18f
Batixx <s59fpern@uni-bonn.de>
2026-02-26 18:04:24 +0100

    restore main.min.json

--- packages/viewer/src/routes/+layout.server.ts
+++ packages/viewer/src/routes/+layout.server.ts
@@ -1,10 +1,9 @@
 import main from '../../public/refs/heads/main.json'
-import undecidable from '../../public/refs/heads/undecidable-data.json'
 
 const bundleHost = import.meta.env.VITE_BUNDLE_HOST
 
 export const load = () => {
   if (bundleHost?.includes('localhost')) {
-    return undecidable
+    return main
   }
 }
